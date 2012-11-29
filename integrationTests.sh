62d6c4e57b26f25e5413919ec0da7d1ee4fcc93e
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2012-11-29 09:51:24 +0100

    integration test should now work

--- integrationTests.sh
+++ integrationTests.sh
@@ -1,41 +1,21 @@
 #!/bin/bash
 
+SCRIPT_PATH=$(pwd)
+
 set -o errexit
 
-datadir=$1
+fileServer="https://sbforge.org/downloads/scape/scape_xcorrsound_jenkins_files"
 
-host=scape@iapetus
+mkdir -p "$SCRIPT_PATH/data"
+cd "$SCRIPT_PATH/data"
+wget -r -np -nd -N "$fileServer/"
+cd ..
 
-#sudo apt-get install libboost-dev
-#sudo apt-get install libboost-program-options-dev
+datadir="$SCRIPT_PATH/data"
 
-make clean
-var=$(pwd)
-if [ ! -d "$var/fftw" ]; then 
-        echo "Setting up fftw.."
-        cd fftw-3.2.2
-        ./configure --prefix=$var/fftw
-        make
-        make install
-        cd ..
-fi
-
-
-make migrationQA
-
-make xcorrSound
-
-make soundMatch
-
-tar -cvzf scape_xcorrsound.tgz migrationQA xcorrSound sound_match *Test.sh >/dev/null
-
-ssh $host "rm -rf working/scape_xcorrsound_jenkins && mkdir working/scape_xcorrsound_jenkins"
-scp scape_xcorrsound.tgz $host:~/working/scape_xcorrsound_jenkins/
-ssh $host "\
-        set -o errexit
-        cd working/scape_xcorrsound_jenkins/; \
-        tar -xvzf scape_xcorrsound.tgz > /dev/null; \
-        for file in *Test.sh; do \
-                ./\$file $datadir; \
-        done;"
+for file in $SCRIPT_PATH/*Test.sh; do 
+   echo 
+   $file "$datadir"
+done;
+      
         
