c62ac894c5abc09c99afa60f00411d2f7c0ad4b5
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2013-05-14 10:43:00 +0200

    Made tests run after rename

--- waveformCompareFail1Test.sh
+++ waveformCompareFail1Test.sh
@@ -4,7 +4,7 @@
 echo "Running fail test 1"
 datadir=$1
 
-command="./migrationQA $datadir/P1_1800_2000_040712_001.mp3.ffmpeg.short.wav $datadir/P1_1800_2000_040712_001.mp3.mpeg321.short.wav --verbose"
+command="./waveform-compare $datadir/P1_1800_2000_040712_001.mp3.ffmpeg.short.wav $datadir/P1_1800_2000_040712_001.mp3.mpeg321.short.wav --verbose"
 
 echo "Execution \"$command\" on $(hostname)"
 
--- waveformCompareSuccess1Test.sh
+++ waveformCompareSuccess1Test.sh
@@ -5,7 +5,7 @@ echo "Running succes test 1"
 
 datadir=$1
 
-command="./migrationQA $datadir/P1_1800_2000_031001_001.mp3.ffmpeg.wav $datadir/P1_1800_2000_031001_001.mp3.mpg321.wav --verbose"
+command="./waveform-compare $datadir/P1_1800_2000_031001_001.mp3.ffmpeg.wav $datadir/P1_1800_2000_031001_001.mp3.mpg321.wav --verbose"
 
 echo "Execution \"$command\" on $(hostname)"
 
