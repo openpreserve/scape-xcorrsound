5f923e33467a83c668902251689179c53300c391
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2013-07-11 11:21:51 +0200

    moved and fixed integrationTests files

---
+++ tests/waveformCompareFail1Test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+echo "Running fail test 1"
+datadir=$1
+build="../build/apps/"
+
+command="$build/waveform-compare $datadir/P1_1800_2000_040712_001.mp3.ffmpeg.short.wav $datadir/P1_1800_2000_040712_001.mp3.mpeg321.short.wav --verbose"
+
+echo "Execution \"$command\" on $(hostname)"
+
+$command
+returncode=$?
+if [ $returncode -eq 1 ]; then
+        echo "Test passed, Failure expected result"
+        exit 0;
+else
+        echo "Test failed, files should not be alike"
+        exit $returncode;
+fi
+
+
---
+++ tests/waveformCompareSuccess1Test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -o errexit
+echo "Running succes test 1"
+
+datadir=$1
+build="../build/apps/"
+
+command="$build/waveform-compare $datadir/P1_1800_2000_031001_001.mp3.ffmpeg.wav $datadir/P1_1800_2000_031001_001.mp3.mpg321.wav --verbose"
+
+echo "Execution \"$command\" on $(hostname)"
+
+$command
+
+echo "Test passed"
+
